88709d11656fe4d7fd44c84b887f517e50b6f990
vrkx <xvorkixy@gmail.com>
2024-12-18 20:22:44 -0400

    UI REVAMP

--- PingniteUI/MainWindow.xaml.cs
+++ PingniteUI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Net;
 using System.Net.NetworkInformation;
 using System.Reflection;
 using System.Security.Policy;
@@ -25,51 +26,87 @@ namespace PingniteUI
 
         public MainWindow()
         {
+            // Start app
             InitializeComponent();
+            // Ping servers and get information about them.
+
+            // EAST
             Ping pingea = new Ping();
             PingReply replyea = pingea.Send("ping-nae.ds.on.epicgames.com", 1000);
-            NaIP.Content = "Ping : " + replyea.RoundtripTime + "ms";
-            NaMS.Content = "Address : " + replyea.Address;
+            ServerN.Content = "NA-EAST";
+            NaServer.Content = "ping-nae.ds.on.epicgames.com";
+
+            NaMS.Content = replyea.RoundtripTime + "MS";
             NaSTATUS.Content = replyea.Status;
+
 
             Ping pingwe = new Ping();
             PingReply replywe = pingwe.Send("ping-naw.ds.on.epicgames.com", 1000);
-            NaIP1.Content = "Ping : " + replywe.RoundtripTime + "ms";
-            NaMS1.Content = "Address : " + replywe.Address;
+            ServerN1.Content = "NA-WEST";
+            NaServer1.Content = "ping-naw.ds.on.epicgames.com";
+
+            NaMS1.Content = replywe.RoundtripTime + "MS";
             NaSTATUS1.Content = replywe.Status;
 
             Ping pingnac = new Ping();
             PingReply replynac = pingnac.Send("ping-nac.ds.on.epicgames.com", 1000);
-            NaIP2.Content = "Ping : " + replynac.RoundtripTime + "ms";
-            NaMS2.Content = "Address : " + replynac.Address;
+            ServerN2.Content = "NA-CENTRAL";
+            NaServer2.Content = "ping-nac.ds.on.epicgames.com";
+
+            NaMS2.Content = replynac.RoundtripTime + "MS";
             NaSTATUS2.Content = replynac.Status;
 
             Ping pingbr = new Ping();
             PingReply replybr = pingbr.Send("ping-br.ds.on.epicgames.com", 1000);
-            NaIP3.Content = "Ping : " + replybr.RoundtripTime + "ms";
-            NaMS3.Content = "Address : " + replybr.Address;
+            ServerN3.Content = "BRAZIL";
+            NaServer3.Content = "ping-br.ds.on.epicgames.com";
+
+            NaMS3.Content = replybr.RoundtripTime + "MS";
             NaSTATUS3.Content = replybr.Status;
 
 
             Ping pingeu = new Ping();
             PingReply replyeu = pingeu.Send("ping-eu.ds.on.epicgames.com", 1000);
-            NaIP4.Content = "Ping : " + replyeu.RoundtripTime + "ms";
-            NaMS4.Content = "Address : " + replyeu.Address;
+            ServerN4.Content = "EUROPE";
+            NaServer4.Content = "ping-eu.ds.on.epicgames.com";
+
+            NaMS4.Content = replyeu.RoundtripTime + "MS";
             NaSTATUS4.Content = replyeu.Status;
 
 
             Ping pingme = new Ping();
             PingReply replyme = pingme.Send("ping-me.ds.on.epicgames.com", 1000);
-            NaIP5.Content = "Ping : " + replyme.RoundtripTime + "ms";
-            NaMS5.Content = "Address : " + replyme.Address;
+            ServerN5.Content = "MIDDLE-EAST";
+            NaServer5.Content = "ping-me.ds.on.epicgames.com";
+
+            NaMS5.Content = replyme.RoundtripTime + "MS";
             NaSTATUS5.Content = replyeu.Status;
+
+
+            // Testing Servers and Changing Colors to the Embeds if servers are up or down
+            // Not working fixing soon!
+
+            if (NaSTATUS.Content.ToString() == "Success")
+            {
+
+
+                Console.WriteLine("Servers Up!");
+            }
+            else if (NaSTATUS.Content.ToString() == "Error")
+            {
+
+                Console.WriteLine("Servers Down :(");
+
+            }
+
+            // Uodate
 
         }
 
 
 
 
-
+        // App drag when long click
         protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
         {
             base.OnMouseLeftButtonDown(e);
@@ -78,56 +115,75 @@ namespace PingniteUI
             this.DragMove();
         }
 
+        // Github btn
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             
             Process.Start("explorer", "http://github.com/vrkx/PingniteUI"); 
         }
 
+        // Close app
+
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
         }
 
+        // Refresh servers info
+
         private void Reload_Click(object sender, RoutedEventArgs e)
         {
             Task.Delay(500).Wait();
             Ping pingea = new Ping();
-            PingReply replyea = pingea.Send("ping-nae.ds.on.epicgames.com", 1000); 
-            NaIP.Content = "Ping : " + replyea.RoundtripTime + "ms";
-            NaMS.Content = "Address : " + replyea.Address;
+            PingReply replyea = pingea.Send("ping-nae.ds.on.epicgames.com", 1000);
+            ServerN.Content = "NA-EAST";
+            NaServer.Content = "ping-nae.ds.on.epicgames.com";
+           
+            NaMS.Content =  replyea.RoundtripTime + "MS";
             NaSTATUS.Content = replyea.Status;
+            
 
             Ping pingwe = new Ping();
             PingReply replywe = pingwe.Send("ping-naw.ds.on.epicgames.com", 1000);
-            NaIP1.Content = "Ping : " + replywe.RoundtripTime + "ms";
-            NaMS1.Content = "Address : " + replywe.Address;
+            ServerN1.Content = "NA-WEST";
+            NaServer1.Content = "ping-naw.ds.on.epicgames.com";
+           
+            NaMS1.Content =   replywe.RoundtripTime + "MS";
             NaSTATUS1.Content = replywe.Status;
 
             Ping pingnac = new Ping();
             PingReply replynac = pingnac.Send("ping-nac.ds.on.epicgames.com", 1000);
-            NaIP2.Content = "Ping : " + replynac.RoundtripTime + "ms";
-            NaMS2.Content = "Address : " + replynac.Address;
+            ServerN2.Content = "NA-CENTRAL";
+            NaServer2.Content = "ping-nac.ds.on.epicgames.com";
+          
+            NaMS2.Content =   replynac.RoundtripTime + "MS";
             NaSTATUS2.Content = replynac.Status;
 
             Ping pingbr = new Ping();
             PingReply replybr = pingbr.Send("ping-br.ds.on.epicgames.com", 1000);
-            NaIP3.Content = "Ping : " + replybr.RoundtripTime + "ms";
-            NaMS3.Content = "Address : " + replybr.Address;
+            ServerN3.Content = "BRAZIL";
+            NaServer3.Content = "ping-br.ds.on.epicgames.com";
+         
+            NaMS3.Content =   replybr.RoundtripTime + "MS";
             NaSTATUS3.Content = replybr.Status;
 
 
             Ping pingeu = new Ping();
             PingReply replyeu = pingeu.Send("ping-eu.ds.on.epicgames.com", 1000);
-            NaIP4.Content = "Ping : " + replyeu.RoundtripTime + "ms";
-            NaMS4.Content = "Address : " + replyeu.Address;
+            ServerN4.Content = "EUROPE";
+            NaServer4.Content = "ping-eu.ds.on.epicgames.com";
+        
+            NaMS4.Content =   replyeu.RoundtripTime + "MS";
             NaSTATUS4.Content = replyeu.Status;
 
 
             Ping pingme = new Ping();
             PingReply replyme = pingme.Send("ping-me.ds.on.epicgames.com", 1000);
-            NaIP5.Content = "Ping : " + replyme.RoundtripTime + "ms";
-            NaMS5.Content = "Address : " + replyme.Address;
+            ServerN5.Content = "MIDDLE-EAST";
+            NaServer5.Content = "ping-me.ds.on.epicgames.com";
+    
+            NaMS5.Content =   replyme.RoundtripTime +"MS";
             NaSTATUS5.Content = replyeu.Status;
 
             
@@ -135,5 +191,17 @@ namespace PingniteUI
 
 
         }
+
+        // Discord Invite ( Moving Soon )
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+            Process.Start("explorer", "https://discord.gg/MWhmZRDU");
+
+        }
+
+
+
     }
 }
